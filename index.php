0a07f7da067739e8e3987a31deb6903b94bcdc7c
Bastien LACOUR <blacour@lerebours.org>
2022-02-14 10:15:09 +0100

    Start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="./CSS/Styles.css" rel="stylesheet">
+    <script type="text/javascript" src="./JQuery/JQuery 3.5.1.js"></script>
+    <script type="text/javascript" src="./JS/Fonctions.js"></script>
+    <script type="text/javascript" src="./JQuery/Chart.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
