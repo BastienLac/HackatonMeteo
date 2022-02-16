29d7cb231f8fbb80a35131bc44b0a5da88fea65d
skyloRen <95213026+skyloRen@users.noreply.github.com>
2022-02-16 09:43:56 +0100

    ok

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     <link href="./CSS/Styles.css" rel="stylesheet">
     <script type="text/javascript" src="./JQuery/JQuery 3.5.1.js"></script>
-    <script type="text/javascript" src="./JS/Fonctions.js"></script>
+    <script type="text/javascript" src="./JS/fonctions.js"></script>
     <script type="text/javascript" src="./JQuery/Chart.js"></script>
     <script src="./Bootstrap/js/bootbox.min.js"></script>
     <script src="./Bootstrap/js/bootstrap.min.js"></script>
@@ -40,15 +40,15 @@
     </div>
     <div class="card text-center">
         <h5 class="card-header">Bienvenue, veuillez vous connecter</h5>
-            <div class="card-body">
-                        <label>Login</label><br />
-                        <input type="text" id="txtLogin"><br /><br/>
-                        <label>Mot de passe</label><br />
-                        <input type="text" id="txtMdp"><br /><br/>
-                        <input class="btn btn-primary" style="width: 180px" type="submit" value="Envoyer" name="btnEnvoyer" onclick="VerifierConnexion()">
-                        <br>
-                        Pas de compte?<a href="PHP/inscription.php">S'inscrire</a> 
-            </div>
+        <div class="card-body">
+                <label>Login</label><br />
+                <input type="text" id="txtLogin"><br /><br/>
+                <label>Mot de passe</label><br />
+                <input type="text" id="txtMdp"><br /><br/>
+                <input class="btn btn-primary" style="width: 180px" type="submit" value="Envoyer" name="btnEnvoyer" onclick="VerifierConnexion()">
+                <br>
+                Pas de compte?<a href="PHP/inscription.php">S'inscrire</a> 
+        </div>
     </div>       
 </body>
 </html>
